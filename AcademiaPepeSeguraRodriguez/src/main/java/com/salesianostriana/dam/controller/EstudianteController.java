6f02f4827b32ecc80c05fcfd7be2d36f1e3591e8
José Rafael <pepesegurarodriguez@gmail.com>
2024-05-08 14:19:16 +0200

    Eliminación de errores

--- AcademiaPepeSeguraRodriguez/src/main/java/com/salesianostriana/dam/controller/EstudianteController.java
+++ AcademiaPepeSeguraRodriguez/src/main/java/com/salesianostriana/dam/controller/EstudianteController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.salesianostriana.dam.model.Estudiante;
@@ -40,14 +41,36 @@ public class EstudianteController {
 	@PostMapping("/nuevo/submit")
 	public String procesarFormulario(@ModelAttribute("estudiante") Estudiante e) {
 		estudianteService.save(e);
-		
 		return "rediret:/";
 	}
 	
-	@GetMapping("/editar/estudiante")
-	public String getMethodName(@RequestParam String param) {
-		return new String();
+	@GetMapping("/editar/{id}")
+	public String mostrarFormularioEdicion(@PathVariable("id") long id, Model model) {
+		
+		if(estudianteService.findById(id).isPresent()) {
+			model.addAttribute("estudiante", estudianteService.findById(id).get());
+			return "Formulario";
+		}else {
+			return "redirect:/";
+		}
 	}
+	@PostMapping("/editar/submit")
+	public String procesarFormularioEdicion(@ModelAttribute("estudiante") Estudiante e) {
+		estudianteService.edit(e);
+		return "redirect:/";
+	}
+	/**
+	 * Método para borrar 
+	 * @param id
+	 * @return 
+	 */
+	@GetMapping("/borrar/{id}")
+	public String borrar(@PathVariable("id") long id) {
+		estudianteService.deleteById(id);
+		return "redirect:/";
+	}
+	
+	 
 	
 	
 }
