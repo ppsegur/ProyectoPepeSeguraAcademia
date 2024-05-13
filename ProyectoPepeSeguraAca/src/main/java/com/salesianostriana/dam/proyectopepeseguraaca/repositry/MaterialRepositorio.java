317a63c1c747b3dfe32af2feec64896d8f116081
ppsegur <pepesegurarodriguez@gmail.com>
2024-05-14 01:08:47 +0200

    Eliminación paquete errata

--- ProyectoPepeSeguraAca/src/main/java/com/salesianostriana/dam/proyectopepeseguraaca/repositry/MaterialRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.salesianostriana.dam.proyectopepeseguraaca.repositry;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.salesianostriana.dam.proyectopepeseguraaca.model.Material;
-
-public interface MaterialRepositorio extends JpaRepository<Material, Long> {
-
-}
