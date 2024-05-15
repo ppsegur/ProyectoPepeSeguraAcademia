3a1401e3ed5a385d5216b67bf3d41a05967ff6fe
José Rafael <pepesegurarodriguez@gmail.com>
2024-05-15 10:24:26 +0200

    Agregación d elas rutas coorectas en le index del administrador

--- ProyectoPepeSeguraAca/src/main/resources/import.sql
+++ ProyectoPepeSeguraAca/src/main/resources/import.sql
@@ -6,21 +6,14 @@ INSERT INTO Curso (id_curso,nombre,nivel_dificultad, precio) VALUES(1,'Ingles a1
 
 ALTER SEQUENCE Curso_seq RESTART WITH 51;
 
-INSERT INTO Usuario(id,username,contrasena,nombre,apellidos,correo,dni) 
-VALUES (1,'pepe321', 'pepe123', 'Pepe', 'González Pérez', 'pepe@example.com', '12345678A');
+INSERT INTO Usuario(id,username,contrasena,nombre,apellidos,correo,dni) VALUES (1,'pepe321', 'pepe123', 'Pepe', 'González Pérez', 'pepe@example.com', '12345678A');
 
-INSERT INTO   Usuario (id,username,contrasena,nombre,apellidos,correo,dni)
-VALUES (2, 'marialoma', 'contraseña2', 'María', 'López Martínez', 'maria@example.com', '87654321B');
+INSERT INTO   Usuario (id,username,contrasena,nombre,apellidos,correo,dni) VALUES (2, 'marialoma', 'contraseña2', 'María', 'López Martínez', 'maria@example.com', '87654321B');
 
-ALTER SEQUENCE Usuario_seq RESTART WITH 51;
+INSERT INTO Estudiante (id, telefono) VALUES (1, '123456789');
 
-INSERT INTO Estudiante (id, telefono) 
-VALUES (1, '123456789');
+INSERT INTO Estudiante (id, telefono) VALUES (2, '987654321');
 
-INSERT INTO Estudiante (id, telefono) 
-VALUES (2, '987654321');
-
-ALTER SEQUENCE Estudiante_seq RESTART WITH 51;
 
 
 
