7efc592d1b9371a52d8b2210daa6ce2fc3af0a57
José Rafael <pepesegurarodriguez@gmail.com>
2024-05-21 14:13:38 +0200

    Arreglo de errrores

--- src/main/java/com/salesianostriana/dam/proyectopepesegura/repositorio/CertificadoRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.salesianostriana.dam.proyectopepesegura.repositorio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.salesianostriana.dam.proyectopepesegura.modelo.Certificado;
-
-public interface CertificadoRepositorio extends JpaRepository<Certificado, Long>{
-
-}
