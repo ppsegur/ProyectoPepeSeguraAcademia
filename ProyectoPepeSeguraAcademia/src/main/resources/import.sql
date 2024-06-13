da04cb2251647df17945a2d52ade9575f7aa82fb
José Rafael <pepesegurarodriguez@gmail.com>
2024-06-13 14:15:11 +0200

    Finalización de favoritos, estilos mejorados, inicio de investigación del remember-Me. Futuras:(Remember-me, validación de formularios, consultas de  buscar,manejo Excepciones, consultas logicas de negocio(los 3 productos mas vendidos example)) y investigación

--- ProyectoPepeSeguraAcademia/src/main/resources/import.sql
+++ ProyectoPepeSeguraAcademia/src/main/resources/import.sql
@@ -3,18 +3,18 @@
 INSERT INTO Material ( id_material ,nombre, tipo, url ) VALUES(1,'material a1 ingles','video','https://youtu.be/Jzc4HDOKTKg?si=XGFRehqwEeqfgv99');
 ALTER SEQUENCE Material_seq RESTART WITH 51;
 
-INSERT INTO Curso (id_curso,nombre,nivel_dificultad, precio, comprado) VALUES(1,'Ingles', 'a1', 25.00, false);
-INSERT INTO Curso (id_curso,nombre,nivel_dificultad, precio, comprado) VALUES(2,'Ingles', 'a2', 25.00, false);
-INSERT INTO Curso (id_curso,nombre,nivel_dificultad, precio, comprado) VALUES(3,'Ingles', 'b1', 40.00, false);
-INSERT INTO Curso (id_curso,nombre,nivel_dificultad, precio, comprado) VALUES(4,'Ingles', 'b2', 40.00, false);
-INSERT INTO Curso (id_curso,nombre,nivel_dificultad, precio, comprado) VALUES(5,'Frances', 'a1', 25.00, false);
-INSERT INTO Curso (id_curso,nombre,nivel_dificultad, precio, comprado) VALUES(6,'Frances', 'a2', 25.00, false);
-INSERT INTO Curso (id_curso,nombre,nivel_dificultad, precio, comprado) VALUES(7,'Español', 'a1', 25.00, false);
-INSERT INTO Curso (id_curso,nombre,nivel_dificultad, precio, comprado) VALUES(8,'Español', 'a2', 25.00, false);
-INSERT INTO Curso (id_curso,nombre,nivel_dificultad, precio, comprado) VALUES(9,'Español', 'b1', 40.00, false);
-INSERT INTO Curso (id_curso,nombre,nivel_dificultad, precio, comprado) VALUES(10,'Español', 'b2', 40.00, false);
-INSERT INTO Curso (id_curso,nombre,nivel_dificultad, precio, comprado) VALUES(11,'Frances', 'b1', 40.00, false);
-INSERT INTO Curso (id_curso,nombre,nivel_dificultad, precio, comprado) VALUES(12,'Frances', 'b2', 40.00, false);
+INSERT INTO Curso (id_curso,nombre,nivel_dificultad, duracion, precio, comprado) VALUES(1,'Ingles', 'a1', '2 semanas' ,25.00, false);
+INSERT INTO Curso (id_curso,nombre,nivel_dificultad duracion, precio, comprado) VALUES(2,'Ingles', 'a2', '2 semanas' ,25.00, false);
+INSERT INTO Curso (id_curso,nombre,nivel_dificultad, duracion, precio, comprado) VALUES(3,'Ingles', 'b1', '2 semanas', 40.00, false);
+INSERT INTO Curso (id_curso,nombre,nivel_dificultad, duracion, precio, comprado) VALUES(4,'Ingles', 'b2', '2 semanas' ,40.00, false);
+INSERT INTO Curso (id_curso,nombre,nivel_dificultad, duracion, precio, comprado) VALUES(5,'Frances', 'a1','2 semanas' , 25.00, false);
+INSERT INTO Curso (id_curso,nombre,nivel_dificultad, duracion, precio, comprado) VALUES(6,'Frances', 'a2','2 semanas' , 25.00, false);
+INSERT INTO Curso (id_curso,nombre,nivel_dificultad, duracion, precio, comprado) VALUES(7,'Español', 'a1','2 semanas' , 25.00, false);
+INSERT INTO Curso (id_curso,nombre,nivel_dificultad, duracion, precio, comprado) VALUES(8,'Español', 'a2','2 semanas' , 25.00, false);
+INSERT INTO Curso (id_curso,nombre,nivel_dificultad, duracion, precio, comprado) VALUES(9,'Español', 'b1','2 semanas' , 40.00, false);
+INSERT INTO Curso (id_curso,nombre,nivel_dificultad, duracion, precio, comprado) VALUES(10,'Español', 'b2','2 semanas' , 40.00, false);
+INSERT INTO Curso (id_curso,nombre,nivel_dificultad, duracion, precio, comprado) VALUES(11,'Frances', 'b1','2 semanas' , 40.00, false);
+INSERT INTO Curso (id_curso,nombre,nivel_dificultad, duracion, precio, comprado) VALUES(12,'Frances', 'b2','2 semanas' , 40.00, false);
 ALTER SEQUENCE Curso_seq RESTART WITH 62;
 
 INSERT INTO Estudiante(id,username,password,nombre,apellidos,correo,dni,fecha_nacimiento,genero,no_estudiante) VALUES(1,'pepe321', '{bcrypt}$2a$12$958YgJdm4acEfwUiIwNANuxFHl0CHeuZh0KBkbhuYtm.RGIa54YvG', 'Pepe', 'González Pérez', 'pepe@gmail.com', '12345678A','2003-03-01', 'Masculino', true);
